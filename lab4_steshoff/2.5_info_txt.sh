542214319f64a8279746fbf992e6ff40406abfe3
steshoff1 <90890256+steshoff1@users.noreply.github.com>
2022-12-24 01:35:55 +0300

    fixed

--- lab4_steshoff/2.5_info_txt.sh
+++ lab4_steshoff/2.5_info_txt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-find ~ -name "*.txt"
-cat `find ~ -name "*.txt"` | wc -c
-cat `find ~ -name "*.txt"` | wc -l
+
+find ~ -type f -name "*.txt" > /tmp/some.txt
+cat /tmp/some.txt | xargs du -bc 2>/dev/null | tail -1 | cut -f1
+cat /tmp/some.txt | xargs wc -l 2>/dev/null | tail -1 | awk '{print $1}'
+rm /tmp/some.txt
